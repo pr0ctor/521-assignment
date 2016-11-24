4292ef44554ba77cc74362cf617ea10d0cb6b045
pr0ctor <paynecr1@gmail.com>
2016-11-24 18:51:56 -0500

    Added Sql stuff
    added the schemas and some insert statements to start to create the
    database

---
+++ sql/createschema.sql
@@ -0,0 +1,85 @@
+drop table Orders;
+drop table Users;
+drop table Products;
+drop table Inventory;
+drop table Shipment;
+drop table Category;
+drop table States;
+drop table Sizes;
+
+create table Orders 
+(
+	O_ID int not null auto_increment,
+	U_ID int foreign key references Users(U_ID),
+	P_ID int foreign key References Product(P_ID),
+	purchaseDate timestamp,
+	processed boolean default false,
+	primary key(O_ID,U_ID,P_ID)
+);
+
+create table Users
+(
+	U_ID int not null auto_increment,
+	email char(50) not null, --foreign key references Login(email) not null,
+	password varchar(64) not null,
+	fname varchar(100),
+	lname varchar(100),
+	address varchar(100),
+	city varchar(100),
+	add_state char(2) foreign key references States(abbr),
+	accesslevel int default 10,
+	primary key(U_ID)
+);
+
+create table Products
+(
+	P_ID int not null auto_increment, --foreign key references Product(P_ID),
+	name varchar(100),
+	category varchar(50) foreign key references Category(category),
+	metadata varchar(200),
+	brand varchar(50),
+	image varchar(50),
+	price int,
+	description text,
+	gender char(1),
+	primary key(P_ID)
+);
+
+create table Inventory
+(
+	P_ID foreign key references Product(P_ID),
+	quantity int default 10
+);
+
+create table Shipment
+(
+	O_ID int foreign key references Orders(O_ID),
+	U_ID int key references Users(U_ID),
+	address varchar(100) foreign key references Users(address),
+	primary key(O_ID,U_ID)
+);
+
+create table Category
+(
+	category varchar(50),
+	gender char(1),
+	primary key(category,gender)
+);
+
+create table States
+(
+	abbr char(2),
+	primary key(abbr)
+);
+
+create table Sizes
+(
+	sizes varchar(20),
+	category varchar(50) foreign key references Category(category),
+	primary key(sizes,category)
+);
+
+CREATE OR REPLACE LowInventoryView AS
+	SELECT Product.P_ID,name,category,brand,quantity
+	FROM Product p, Inventory i
+	WHERE i.quantity =< 5;
